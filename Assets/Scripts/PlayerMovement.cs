f604904db00798be612ef442bf0405777cec974c
Charlie <charlieryancurry@gmail.com>
2025-01-25 11:25:22 -0400

    testing with cinemachine

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float movementX;
+
+    private CharacterController characterController;
+
+    private Vector3 velocity;
+
+    public float speed = 6.0f;
+
+    public float distance = 1.05f;
+    private bool isGrounded;
+    [SerializeField] private float gravity = -2;
     // Start is called before the first frame update
     void Start()
     {
